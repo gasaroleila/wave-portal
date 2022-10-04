7d8982c63415348e88a2b30f7bcf096566f4ebc1
gasaroleila <uwamgaleila@gmail.com>
2022-10-04 21:24:31 +0200

    Initial setup

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,9 @@
+//  SPDX-License-Identifier: UNLICENSED
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("First contract, yee!");
+    }
+}
---
+++ scripts/run.js
@@ -0,0 +1,21 @@
+
+
+const main = async () => {
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy();
+    await waveContract.deployed();
+    console.log("waveContract deployed at", waveContract.address);
+}
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0)
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+
+runMain();
