5ccf61e9872da3cebb410f01a625958d63321df3
gasaroleila <uwamgaleila@gmail.com>
2022-10-06 15:04:14 +0200

    updated contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,8 +8,8 @@ const main = async () => {
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
-  
     console.log("WavePortal address: ", waveContract.address);
+  
   };
   
   const runMain = async () => {
--- scripts/run.js
+++ scripts/run.js
@@ -1,31 +1,35 @@
-
-
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
-
-    console.log("waveContract deployed at", waveContract.address);
-    console.log("Contract deployed by", owner);
-
-    await waveContract.getTotalWaves();
-
-    const waveTxn = await waveContract.wave()
-    await waveTxn.wait();
-
-    await waveContract.getTotalWaves();
-}
-
-const runMain = async () => {
+    console.log("Contract addy:", waveContract.address);
+  
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+    console.log(waveCount.toNumber());
+  
+    /**
+     * Let's send a few waves!
+     */
+    let waveTxn = await waveContract.wave("A message!");
+    await waveTxn.wait(); // Wait for the transaction to be mined
+  
+    const [_, randomPerson] = await hre.ethers.getSigners();
+    waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+    await waveTxn.wait(); // Wait for the transaction to be mined
+  
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
+  };
+  
+  const runMain = async () => {
     try {
-        await main();
-        process.exit(0)
+      await main();
+      process.exit(0);
     } catch (error) {
-        console.log(error);
-        process.exit(1);
+      console.log(error);
+      process.exit(1);
     }
-}
-
-
-runMain();
+  };
+  
+  runMain();
